9cfcdc34176cc895f9f965dbdbeb7393352535f4
Harshit Pant <133621508+Harshitpant12@users.noreply.github.com>
2025-10-23 19:52:21 +0530

    Adding more features

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
+import { countryNames } from "./cocuntry-names.js"
 document.addEventListener('DOMContentLoaded', () => {
     const cityInput = document.getElementById("city-input")
     const getWeatherBtn = document.getElementById("get-weather-btn")
     const weatherInfo = document.getElementById("weather-info")
-    const cityNameDisplay = document.getElementById("city-name")
+    const cityNameDisplay = document.getElementById("city-text")
+    const weatherIcon = document.getElementById("weatherIcon");
     const temperatureDisplay = document.getElementById("temperature")
+    const weatherDisplay = document.getElementById("weather")
+    const displayHumidity = document.getElementById("humidity")
+    const minTempDisplay = document.getElementById("min-temp")
+    const maxTempDisplay = document.getElementById("max-temp")
     const descriptionDisplay = document.getElementById("description")
-    const errorMessage = document.getElementById("error-message");
+    const errorMessage = document.getElementById("error-message")
 
-    const API_KEY = "YOUR_API_KEY_HERE"; //env variables
+    const API_KEY = "ENTER_YOUR_OPENWEATHER_API_KEY"; //env variables
 
     getWeatherBtn.addEventListener('click', async () => {
         const city = cityInput.value.trim()
@@ -43,10 +49,18 @@ document.addEventListener('DOMContentLoaded', () => {
 
     function displayWeatherData(data){
         console.log(data);
-        const {name, main, weather} = data
-        cityNameDisplay.textContent = `${name}`
+        const  {name, main, weather, sys} = data
+        cityNameDisplay.textContent = `${name}, ${countryNames[sys.country] || sys.country}`
+        const iconUrl = `http://openweathermap.org/img/wn/${weather[0].icon}@2x.png`
+        weatherIcon.src = iconUrl;
+        weatherIcon.alt = weather[0].description
         temperatureDisplay.textContent = `Temperature : ${main.temp}`
-        descriptionDisplay.textContent = `Weather : ${weather[0].description}`
+        weatherDisplay.textContent = `Weather : ${weather[0].description}`
+        displayHumidity.textContent = `Humidity : ${main.humidity}`
+        minTempDisplay.textContent = `Minimum Temperature: ${main.temp_min}`
+        maxTempDisplay.textContent = `Maximum Temperature: ${main.temp_max}`
+        descriptionDisplay.textContent = `So the weather can be described as ${weather[0].description} and feels like ${main.feels_like}`
+
 
         //unlocking display
         weatherInfo.classList.remove('hidden')
